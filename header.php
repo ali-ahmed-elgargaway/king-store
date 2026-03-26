29e531719bf57cdfcd1f22b2d15a5cd75c9cdd79
ali-ahmed-elgargaway <ali.ahmed.23049@gmail.com>
2026-03-27 00:19:20 +0200

    Update header.php

--- header.php
+++ header.php
@@ -14,7 +14,7 @@ if ($row && $row['total_items']) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>King Store</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/style.css" rel="stylesheet">
+    <link href="style.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 </head>
 <body class="d-flex flex-column min-vh-100">
